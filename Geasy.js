6d9e82cc960880d4cf82e3226dbb83fd2753f2f6
Veryei <verb1416@gmail.com>
2024-07-29 19:53:26 -0700

    Update Geasy.js

--- Geasy.js
+++ Geasy.js
@@ -147,7 +147,7 @@ function updateTimer() {
     if (timeLeft <= 0) {
         clearInterval(timerInterval);
         timerElement.innerHTML = 'Time’s up!';
-         nextButton.style.display = "block"; // Automatically submit quiz when time is up
+         nextButton.innerHTML = "Next"; // Automatically submit quiz when time is up
         return;
     }
 
